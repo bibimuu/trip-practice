f90066da279320dafb9d6e2d8711e27801febecf
Daigoro <h.dai56@gmail.com>
2019-04-25 05:36:47 +0000

    ボタンにhoverの機能追加

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('a').hover(
+  function() {
+      $('a').css('background-color', '#f89238');
+  },
+  function() {
+      $('a').css('background-color', '#f0a462a6');
+  });
+});

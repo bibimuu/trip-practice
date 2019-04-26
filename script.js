7ea6d2a10d473cb9c2faf5d6ce71a753ee9b740f
Daigoro <h.dai56@gmail.com>
2019-04-26 03:15:51 +0000

    company-logoとhedder-listの追加

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-  $('a').hover(
-  function() {
-      $('a').css('background-color', '#f89238');
-  },
-  function() {
-      $('a').css('background-color', '#f0a462a6');
+  $('.btn').hover(function() {
+    $(this).css('background-color', '#f89238');
+  }, function() {
+    $(this).css('background-color', '#f0a462a6');
   });
+
+  $('.header-list li').hover(function() {
+    $(this).css('border-bottom', '#f89238 solid 3px');
+  }, function() {
+    $(this).css('border-bottom', 'none');
+  });
+  
 });
